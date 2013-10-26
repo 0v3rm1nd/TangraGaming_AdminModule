7cc5559e697f169baa95ff76d9727efddd5e18e2
Marto <Marto@Marto-PC>
2013-10-26 18:25:48 +0200

    Intitial Submit--> No deisgn, login works, list users works, delete users works, update users works 1/2

--- includes/menu.inc.php
+++ includes/menu.inc.php
@@ -1,17 +1,19 @@
 <ul>
    <li class='active'><a href='../../authenticate/admin/admin_home.php'><span>Home</span></a></li>
-   <li class='has-sub'><a href='#'><span>Products</span></a>
+   <li class='has-sub'><a href='#'><span>User Management</span></a>
       <ul>
-         <li><a href='#'><span>Widgets</span></a></li>
-         <li><a href='#'><span>Menus</span></a></li>
+         <li><a href='#'><span>Group Management</span></a></li>
+         <li><a href='#'><span>Main Group Management</span></a></li>
          <li class='last'><a href='#'><span>Products</span></a></li>
       </ul>
    </li>
-   <li class='has-sub'><a href='#'><span>Company</span></a>
+   <li class='active'><a href='../../authenticate/admin/admin_home.php'><span>Group Management</span></a></li>
+   <li class='active'><a href='../../authenticate/admin/admin_home.php'><span>Main Group Management</span></a></li>
+   <li class='has-sub'><a href='#'><span>Statistics</span></a>
       <ul>
          <li><a href='#'><span>About</span></a></li>
          <li class='last'><a href='#'><span>Location</span></a></li>
       </ul>
    </li>
-   <li class='last'><a href='#'><span>Contact</span></a></li>
+   <li class='last'><a href='#'><span>Logout</span></a></li>
 </ul>
