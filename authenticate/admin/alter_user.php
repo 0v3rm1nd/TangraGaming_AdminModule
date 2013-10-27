e0a0112183659c59772138ee5933d15b66c6ffd3
Marto <Marto@Marto-PC>
2013-10-28 00:02:00 +0100

    Intitial Submit--> No deisgn, login works, list users works, delete users works, update users works 1/2

--- authenticate/admin/alter_user.php
+++ authenticate/admin/alter_user.php
@@ -3,23 +3,31 @@ require_once('../../includes/session_timeout_db.inc.php');
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Update User</title>
-<link rel="stylesheet" type="text/css" href="../../styles/style.css" />
-</head>
-<body>
-<div id="pagesearch1">
-<h1>
-Alter Users
-</h1>
-<a href="admin_home.php">&middot;Admin Home</a>
-<h3>
-List of users:
-</h3>
-<?php
-require_once('../../includes/search_user.inc.php');
-?>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>Update User</title>
+        <link rel="stylesheet" type="text/css" href="../../style.css" />
+    </head>
+    <body>
+        <div id="allcontent">
+            <div id="header">
+                <img src="../../images/banner.png" alt="Tangra Gaming"/>
+            </div>
+            <div id="cssmenu">
+                <?php include('../../includes/menu.inc.php'); ?>
+            </div>
+            <h1>
+                Alter Users
+            </h1>
+            <h3>
+                List of users:
+            </h3>
+            <?php
+            require_once('../../includes/admin/user/search_user.inc.php');
+            ?>
+    </body>
+    <div id="footer">
+        <?php include('../../includes/footer.inc.php'); ?>
+    </div>
 </div>
-</body>
 </html>
--- includes/admin/user/search_user.inc.php
+++ includes/admin/user/search_user.inc.php
@@ -6,7 +6,7 @@ $conn = dbConnect('write');
 $col = 'email';
 $dir = 'ASC';
 // create arrays of permitted values
-$columns = array('email','nickname','salt', 'password');
+$columns = array('email','nickname','lastlogin', 'datecreated');
 $direction = array('ASC', 'DESC');
 // if the form has been submitted, use only expected values
 if (isset($_GET['column']) && in_array($_GET['column'], $columns)) {
@@ -16,7 +16,7 @@ if (isset($_GET['direction']) && in_array($_GET['direction'], $direction)) {
   $dir = $_GET['direction'];
 }
 // prepare the SQL query
-$sql = "SELECT email, nickname, salt, password FROM user
+$sql = "SELECT email, nickname, lastlogin, datecreated FROM user
         ORDER BY $col $dir";
 // submit the query and capture the result
 $result = $conn->query($sql) or die($conn->error);
@@ -27,8 +27,8 @@ $result = $conn->query($sql) or die($conn->error);
   <select name="column" id="column">
     <option value="email" <?php if ($col == 'email') echo 'selected'; ?>>Email</option>
     <option value="nickname" <?php if ($col == 'nickname') echo 'selected'; ?>>Nickname</option>
-    <option value="salt" <?php if ($col == 'salt') echo 'selected'; ?>>Salt</option>
-    <option value="password" <?php if ($col == 'password') echo 'selected'; ?>>Password</option>
+    <option value="lastlogin" <?php if ($col == 'lastlogin') echo 'selected'; ?>>Last Login</option>
+    <option value="datecreated" <?php if ($col == 'datecreated') echo 'selected'; ?>>Date Created</option>
   </select>
   <select name="direction" id="direction">
     <option value="ASC" <?php if ($dir == 'ASC') echo 'selected'; ?>>Ascending</option>
@@ -43,8 +43,8 @@ $result = $conn->query($sql) or die($conn->error);
   <tr>
     <th>Email</th>
     <th>Nickname</th>
-    <th>Salt</th>
-    <th>Password</th>
+    <th>Last Login</th>
+    <th>Date Created</th>
     <th></th>
     <th></th>
  </tr>
@@ -52,8 +52,8 @@ $result = $conn->query($sql) or die($conn->error);
   <tr>
 	<td><?php echo $row['email']; ?></td>
     <td><?php echo $row['nickname']; ?></td>    
-    <td><?php echo $row['salt']; ?></td>
-    <td><?php echo $row['password']; ?></td>
+    <td><?php echo $row['lastlogin']; ?></td>
+    <td><?php echo $row['datecreated']; ?></td>
     <td><a href="../../includes/update_user_mysqli.inc.php?email=<?php echo $row['email']; ?>">EDIT</a></td>
     <td><a href="../../includes/delete_user_mysqli.inc.php?email=<?php echo $row['email']; ?>">DELETE</a></td>
   </tr>
